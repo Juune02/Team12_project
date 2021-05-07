7f48dd318ed76e4118f38c57849177bd694ee484
Juune02 <mykj6573@gmail.com>
2021-05-08 00:14:37 +0900

    project12.py was fixed by Juune02!

--- project12.py
+++ project12.py
@@ -9,7 +9,7 @@ from matplotlib import style
 font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name)
 
-print('영원회귀: 블랙서바이벌의 티어 분포도 \n')
+print('영원회귀: 블랙서바이벌의 티어 분포도(솔로 기준) \n')
 webpage = urllib.request.urlopen('https://dak.gg/bser/statistics/tier')
 soup = BeautifulSoup(webpage, 'html.parser')
 tables = soup.select('table')
@@ -17,12 +17,12 @@ table = tables[0]
 table_html = str(table)
 table_df_list = pd.read_html(table_html)
 table_df = table_df_list[0]
+print(table_df)
 list1 = table_df['Tier'].tolist()
 list2 = table_df['Percentage'].tolist()
 L=''.join(list2)
 L2=L.split('%')
 L2.remove('')
-plt.pie(L2,labels=list1,autopct='%1.1f%%')
 
 tier1 = []
 for tier in list1:
@@ -31,15 +31,29 @@ for tier in list1:
 tier1 = [tier.strip("1") for tier in tier1]
 tier1.insert(0,"Immortal")
 tier1.insert(1,"Titan")
-tier_size = [4.143, 38.61, 35.617, 13.939, 5.7, 1.283, 0.246, 0.167]
+tier1.reverse()
+TT1=0
+for s in range(22,26):
+    TT1+=float(L2[s])
+TT2=0
+for s in range(18,22):
+    TT2+=float(L2[s])
+TT3=0
+for s in range(14,18):
+    TT3+=float(L2[s])
+TT4=0
+for s in range(10,14):
+    TT4+=float(L2[s])
+TT5=0
+for s in range(6,10):
+    TT5+=float(L2[s])
+TT6=0
+for s in range(2,6):
+    TT6+=float(L2[s])
+tier_size = [TT1,TT2,TT3,TT4,TT5,TT6,float(L2[1]),float(L2[0])]
 
 width_num = 0.4
-fig, ax = plt.subplots()
-ax.axis('equal')
-pie_outside, _ = ax.pie(tier_size, radius = 1.3, labels = tier1, labeldistance = 0.8)
-plt.setp(pie_outside, width = width_num, edgecolor = 'white')
-pie_inside, plt_labels, junk = \
-ax.pie(L2, radius=(1.3 - width_num), labels=list1, labeldistance=0.75, autopct='%1.1f%%')
-plt.setp(pie_inside, width=width_num, edgecolor='white')
-plt.title('영원회귀: 블랙서바이벌의 티어 분포도', fontsize=30)
+plt.axis('equal')
+plt.pie(tier_size, labels = tier1, autopct='%1.1f%%')
+plt.title('영원회귀: 블랙서바이벌의 티어 분포 그래프(솔로 기준)', fontsize=30)
 plt.show()
